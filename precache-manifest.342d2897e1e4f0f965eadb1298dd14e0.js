cbcf903a20fbfb1ff412207cff47cc0011ce4453
Jakub Gajewski <jacobs.hacking@gmail.com>
2019-05-08 07:35:41 +0100

    Updates

---
+++ precache-manifest.342d2897e1e4f0f965eadb1298dd14e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/cra-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/cra-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/cra-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d325206c3ff117b2a2d3",
+    "url": "/cra-rr/static/js/runtime~main.956f0e41.js"
+  },
+  {
+    "revision": "2eb985746d808772d892",
+    "url": "/cra-rr/static/js/main.89425ee1.chunk.js"
+  },
+  {
+    "revision": "8cc428d45d05896278b3",
+    "url": "/cra-rr/static/js/2.14852d74.chunk.js"
+  },
+  {
+    "revision": "2eb985746d808772d892",
+    "url": "/cra-rr/static/css/main.da794f2f.chunk.css"
+  },
+  {
+    "revision": "a5d6d97e6dfa3c6f659073fde9cfa1ac",
+    "url": "/cra-rr/index.html"
+  }
+];
