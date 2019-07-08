8163cc11ea7d803f7ca5668351d2f9e901ab1c20
Jakub Gajewski <jacobs.hacking@gmail.com>
2019-07-08 08:14:46 +0100

    admin v2

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import {Route, Redirect} from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 
 const permission = false;
 
-const  AdminPage= () => {
-    return ( 
-       <Route render = {() => (
-        permission ? ( <h3> Welcome </h3> ) : (
-        <Redirect to ="/login"/>
-           )
-       )} />
-     );
+const AdminPage = () => {
+  return (
+    <Route render={() => (
+      permission ? (<h3> Welcome </h3>) : (
+        <Redirect to="/login" />
+      )
+    )} />
+  );
 }
- 
-export default AdminPage ;
+
+export default AdminPage;
